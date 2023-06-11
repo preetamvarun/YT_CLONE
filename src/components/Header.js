039fc6220a5885f914c63ad813809d6bc06d8981
Preetam Varun <preetamarun99@gmail.com>
2023-06-11 23:22:46 +0100

    styled header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,40 @@
 import ham from './Images/hamburgermenuicon.jpg';
 import yt_logo from './Images/yt_1200.png';
+import magnifying_glass from './Images/magnifying_glass.jpg';
+import userIcon from './Images/avatar.png';
 
 const Header = () => {
   return (
-    <div className='flex flex-1 items-center bg-red-800'>
+    <div className='flex flex-1 items-center justify-between shadow-lg'>
+      {/* Left Side Two Logos */}
       <div className='flex items-center'>
         <div className='mx-2'>
           <img src={ham} alt='hamburger-menu-icon' className='h-12 w-12' />
         </div>
         <div>
-          <img src={yt_logo} alt='yt_logo' className='h-20 w-24' />
+          <img src={yt_logo} alt='yt_logo' className='h-[70px] w-20' />
         </div>
       </div>
-      <div>
+
+      {/* Search Bar Field */}
+      <div className='flex items-center'>
         <input
           type='text'
           placeholder='search'
-          className='py-2 px-4 rounded-l-3xl border border-solid border-black'
+          className='py-2 px-4 rounded-l-3xl border border-solid border-black w-96 outline-none'
         />
+        <div className='h-10 px-3 rounded-r-3xl bg-slate-50 border border-black'>
+          <img
+            src={magnifying_glass}
+            alt='magnifying_glass'
+            className='h-6 relative top-1/2 translate-y-[-50%]'
+          />
+        </div>
+      </div>
+
+      {/*Profile Pic Field*/}
+      <div className='mr-4'>
+        <img src={userIcon} alt='userIcon' className='h-10' />
       </div>
     </div>
   );
