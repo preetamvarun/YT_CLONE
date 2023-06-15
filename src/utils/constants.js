641d1af20e86cb7c4bd0e4576b9483cd88f2b71a
Preetam Varun <preetamarun99@gmail.com>
2023-06-15 22:56:39 +0100

    get the api key

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const API_KEY = 'AIzaSyCn76zXUdXLcqy4Ik1QwISRFLK307QsbRI';
+
+export const youtubeAPI =
+  'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=' +
+  API_KEY;
