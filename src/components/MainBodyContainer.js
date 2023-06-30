3ece48cca55ecaa070986e8913c055ee443c8304
PREETAM <preetamvarun99@gmail.com>
2023-06-30 12:39:07 +0100

    git commit -m "deleted button component"

--- src/components/MainBodyContainer.js
+++ src/components/MainBodyContainer.js
@@ -1,10 +1,8 @@
-import ButtonsList from './ButtonsList';
 import VideoContainer from './VideoContainer';
 
 const MainBodyContainer = () => {
   return (
     <div style={{ flex: 8 }}>
-      <ButtonsList />
       <VideoContainer />
     </div>
   );
